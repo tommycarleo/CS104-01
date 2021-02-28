da941f6706bb491cc28d485c8ec656e4ff03c333
tommycarleo <79094510+tommycarleo@users.noreply.github.com>
2021-02-28 14:55:00 -0500

    While loop

---
+++ computeAverage.py
@@ -0,0 +1,10 @@
+howManyEntered = 0
+total = 0
+average = 0
+#this line inputs how many test scores user wants to enter
+howMany = int (input ("How many test scores would you like to average? "))
+while (howManyEntered < howMany):
+    testScore = input ("Enter test score")
+    howManyEntered = howManyEntered + 1
+    
+
