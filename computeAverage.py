490678e61c387bf6b5e481d8f6dd5f6d0efe0c30
tommycarleo <79094510+tommycarleo@users.noreply.github.com>
2021-03-02 16:58:19 -0500

    Add files via upload

--- computeAverage.py
+++ computeAverage.py
@@ -3,8 +3,12 @@ total = 0
 average = 0
 #this line inputs how many test scores user wants to enter
 howMany = int (input ("How many test scores would you like to average? "))
-while (howManyEntered < howMany):
-    testScore = input ("Enter test score")
-    howManyEntered = howManyEntered + 1
+while howManyEntered <= howMany:
+    #this line allows user to enter a score and then stores it in testScore
+    testScore = int(input ("Enter test score: "))
+    howManyEntered = howManyEntered 
+    total = total + testScore
+    average = total/howMany
+print ("The average is: " + str(average))
     
 
