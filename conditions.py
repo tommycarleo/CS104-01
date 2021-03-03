2ad0febf51bc963afbc6a09ffae8b22b0eafd68a
tommycarleo <79094510+tommycarleo@users.noreply.github.com>
2021-03-03 08:42:37 -0500

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,25 @@
+temp = 0
+while temp != 999:
+    temp = int(input("Please enter a new temperature or 999 to End"))
+    if temp == 999:
+        break     
+    if temp > 90:
+        print ("Wear Shorts")
+    elif temp > 70:
+        print ("Short sleeves are fine")
+    elif temp > 50:
+        print ("Wear a jacket")
+    elif temp > 32:
+        print ("Wear a heavy coat")
+    elif temp < 32:
+        print ("Stay Inside")
+    
+      
+
+
+
+
+
+
+    
+    
